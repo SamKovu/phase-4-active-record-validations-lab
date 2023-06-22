1009ffc1eb8eae12655b08b1e84e3b5973f25c7b
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-06-22 18:23:46 +0300

    Completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    
+    validate  :title_is_clickbait
+
+    def title_is_clickbait
+        if title.present? && !title.match(/Won't Believe|Secret|Top \d|Guess/)
+                errors.add(:title, "Needs more clickbait!")
+        end
+    end
+
+   
+
 end
